e5ef77f724cedf1ae301863c764f0a30345a0cff
frank boakye yiadom <fbyiadom@gmail.com>
2017-04-08 15:13:35 -0400

    add resources to our application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+	get 'pages/about'
+
+  get 'pages/requirement'
+
+  devise_for :users
+	resources :posts do
+		resources :comments
+	end
+
+	root "posts#index"
   devise_for :users
   devise_for :models
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
