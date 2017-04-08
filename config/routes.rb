39b2ddbdcf0599c368c7e12be8b86879f17abf31
frank boakye yiadom <fbyiadom@gmail.com>
2017-04-08 15:50:32 -0400

    add nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,5 @@ Rails.application.routes.draw do
 	end
 
 	root "posts#index"
-  devise_for :users
-  devise_for :models
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
