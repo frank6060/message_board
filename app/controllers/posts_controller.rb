95e33996b253e661b49721cc8eecd91c1e04cd10
frank boakye yiadom <fbyiadom@gmail.com>
2017-04-08 15:11:01 -0400

    add all the CRUD functionalities for the post and comment controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,56 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
+
+	def new
+		@post = current_user.posts.build
+	end
+
+	def show
+		
+	end
+
+	def create
+		@post = current_user.posts.build(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+
+		if @post.update(params[:post].permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
+	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
 end
