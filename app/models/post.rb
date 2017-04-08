ae14b9b5f35b29ac13205387927d7f7b9d509318
frank boakye yiadom <fbyiadom@gmail.com>
2017-04-08 15:16:36 -0400

    add all the association between the models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+	paginates_per 10
+	
+	belongs_to :user
+	has_many :comments, dependent: :destroy
+	validates :title, presence: true, length: {minimum: 5}
+	validates :body,  presence: true
 end
