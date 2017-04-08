ce9b4c6a5d0667d02f13f27ba51b1b5d0230b225
frank boakye yiadom <fbyiadom@gmail.com>
2017-04-08 15:04:59 -0400

    add migration to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408185849) do
+ActiveRecord::Schema.define(version: 20170408190231) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20170408185849) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.text     "about"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
